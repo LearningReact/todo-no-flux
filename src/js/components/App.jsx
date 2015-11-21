f397e09c73b091e03fdaac94f8b805c93755d4d4
Chris Rinaldi <cgrinaldi@gmail.com>
2015-11-21 14:02:49 -0500

    Render tasks
    Tasks are sent as props to <Tasks /> from <App />. We want to do
    this so that <Tasks /> is a "dumb component" that is only
    responsible for rendering tasks given as input.

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import Tasks from './Tasks.jsx';
 
+var tasks = [
+  {id: 1, task: 'Take out trash'},
+  {id: 2, task: 'Walk the cat'}
+];
+
 module.exports = React.createClass({
   render () {
     return (
       <div>
         <h1>Todo App</h1>
-        <Tasks />
+        <Tasks tasks={tasks} />
       </div>
     );
   }
--- src/js/components/Tasks.jsx
+++ src/js/components/Tasks.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 module.exports = React.createClass({
   render () {
+    var tasks = this.props.tasks.map((item => {
+      return <li className="task" key={item.id}>
+        {item.task}
+      </li>
+    }));
+
     return (
-      <div>
-        My tasks
-      </div>
+      <ul>
+        {tasks}
+      </ul>
     );
   }
 });
